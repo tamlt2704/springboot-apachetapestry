91df6b16afa05e082af39c5c4dbbb06f5ef2cc0f
tamlt2704 <tamlt2704@gmail.com>
2023-10-01 13:13:35 +0800

    refactor index page

--- src/main/java/jumpstart/web/pages/Index.java
+++ src/main/java/jumpstart/web/pages/Index.java
@@ -1,5 +1,8 @@
 package jumpstart.web.pages;
 
+import org.apache.tapestry5.annotations.Import;
+
+@Import(stylesheet = "css/examples/index.css")
 public class Index {
     /** An ordinary getter. */
     public String getUsername() {
